0cb1c6b2ecece23d85b6c31b862173ceed01699d
ivan <example@gmail.com>
2022-01-07 10:24:06 +0300

    first comit

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.log('aee' + " - object");
+console.log(4 + " - object");
+
+let ulo = 20,
+    yoy = 20;
+// ++ulo;
+// --yoy;
+
+console.log(++ulo);
+console.log(--yoy);
+
+console.log(2 + 2 * 2 !== '6');
+
+const ischeked = false, 
+      isclose = false;
+
+console.log(ischeked || !isclose);
